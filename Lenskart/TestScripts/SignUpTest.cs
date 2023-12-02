ab943c6de57acc72d0468606a47a414fab1924b5
indcyr <148056333+indcyr@users.noreply.github.com>
2023-12-02 21:49:55 +0530

    Update SignUpTest.cs

--- Lenskart/TestScripts/SignUpTest.cs
+++ Lenskart/TestScripts/SignUpTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using Lenskart.PageObjects;
+using Lenskart.Utilities;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,50 @@ using System.Threading.Tasks;
 
 namespace Lenskart.TestScripts
 {
-    internal class SignUpTest
+    [TestFixture]
+    internal class SignUpTest : CoreCodes
     {
+        [Test, Order(1)]
+        public void CreateAccountTest()
+        {
+            string? currDir = Directory.GetParent(@"../../../")?.FullName;
+            string filePath = currDir + "/Logs/log_" + DateTime.Now.ToString("yyyy-mm-dd_HH.mm.ss") + ".txt";
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .WriteTo.File(filePath, rollingInterval: RollingInterval.Day).CreateLogger();
+
+            LenskartHomePage lhp = new(driver);
+            string? excelFilePath = currDir + "/TestData/InputData.xlsx";
+            string? sheetName = "Lenskart";
+
+            List<SearchData> excelDataList = ExcelUtils.ReadSearchDataExcelData(excelFilePath, sheetName);
+
+            foreach (var excelData in excelDataList)
+            {
+                try
+                {
+                    string? firstname = excelData?.FirstName;
+                    string? lastname = excelData?.LastName;
+                    string? mobno = excelData?.MobileNo;
+                    string? emailId = excelData?.EmailId;
+                    string? password = excelData?.Password;
+
+                    lhp?.SignUp(firstname, lastname, mobno, emailId, password);
+                    TakeScreenShot();
+                    Assert.That(driver.Url, Does.Contain("lenskart.com"));
+                    LogTestResult("Sign Out Test", "Sign Out Test Passed");
+                    test = extent.CreateTest("Sign Out Test Passed");
+                    test.Pass("Sign Out test Passed");
+                }
+                catch (AssertionException ex)
+                {
+                    TakeScreenShot();
+                    LogTestResult("Sign Out Test", "Sign Out Test failed", ex.Message);
+                    test.Fail("Sign Out test failed");
+                }
+
+            }
+        }
+        
     }
 }
