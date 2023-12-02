fed5755beda961c3c46a3554a6e26f0eb1ce569e
indcyr <148056333+indcyr@users.noreply.github.com>
2023-12-02 21:51:52 +0530

    Update WishListTest.cs

--- Lenskart/TestScripts/WishListTest.cs
+++ Lenskart/TestScripts/WishListTest.cs
@@ -1,4 +1,8 @@
-﻿using System;
+using Lenskart.PageObjects;
+using Lenskart.Utilities;
+using OpenQA.Selenium;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,45 @@ using System.Threading.Tasks;
 
 namespace Lenskart.TestScripts
 {
-    internal class WishListTest
+    [TestFixture]
+    internal class WishListTest : CoreCodes
     {
+        [Test]
+        public void AddToWishList()
+        {
+            string? currDir = Directory.GetParent(@"../../../")?.FullName;
+            string filePath = currDir + "/Logs/log_" + DateTime.Now.ToString("yyyy-mm-dd_HH.mm.ss") + ".txt";
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .WriteTo.File(filePath, rollingInterval: RollingInterval.Day).CreateLogger();
+            LenskartHomePage lhp = new(driver);
+            try
+            {
+                var searchPage = lhp?.ClickSearchProduct();
+                Thread.Sleep(1000);
+                var productPage = searchPage?.SelectEyewear();
+
+                List<string> str = driver.WindowHandles.ToList();
+                driver.SwitchTo().Window(str[1]);
+
+                productPage?.AddToWishList();
+                TakeScreenShot();
+                IWebElement element = driver.FindElement(By.Id("wishlistView"));
+                var wishListClass=element.GetAttribute("class");
+                Assert.That(wishListClass.Contains("hYsszL") == true);
+                LogTestResult("WishList Test", "WishList Test Passed");
+                test = extent.CreateTest("WishList Test Passed");
+                test.Pass("WishList test Passed");
+            }
+            catch (AssertionException ex)
+            {
+                TakeScreenShot();
+                LogTestResult("WishList Test", "WishList Test failed", ex.Message);
+                test.Fail("WishList test failed");
+            }
+
+        }
+
     }
 }
+
