df60949e048cb1cbcc9bb6aea7b5e6eedfa43967
indcyr <148056333+indcyr@users.noreply.github.com>
2023-12-02 21:51:05 +0530

    Update StoreLocatorTest.cs

--- Lenskart/TestScripts/StoreLocatorTest.cs
+++ Lenskart/TestScripts/StoreLocatorTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using Lenskart.PageObjects;
+using Lenskart.Utilities;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,45 @@ using System.Threading.Tasks;
 
 namespace Lenskart.TestScripts
 {
-    internal class StoreLocatorTest
+    [TestFixture]
+    internal class StoreLocatorTest : CoreCodes
     {
+        [Test]
+        public void LocateStore()
+        {
+            string? currDir = Directory.GetParent(@"../../../")?.FullName;
+            string filePath = currDir + "/Logs/log_" + DateTime.Now.ToString("yyyy-mm-dd_HH.mm.ss") + ".txt";
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .WriteTo.File(filePath, rollingInterval: RollingInterval.Day).CreateLogger();
+
+            LenskartHomePage lhp = new(driver);
+            string? excelFilePath = currDir + "/TestData/InputData.xlsx";
+            string? sheetName = "Lenskart";
+
+            List<SearchData> excelDataList = ExcelUtils.ReadSearchDataExcelData(excelFilePath, sheetName);
+
+            foreach (var excelData in excelDataList)
+            {
+                try
+                {
+                    string? city = excelData?.City;
+                    var searchcity = lhp?.ClickOnStoreLocator();
+                    searchcity.StoreSearch(city);
+
+                    TakeScreenShot();
+                    Assert.That(driver.Url, Does.Contain("stores"));
+                    LogTestResult("Store Locator Test", "Store Locator Test Passed");
+                    test = extent.CreateTest("Store Locator Test Passed");
+                    test.Pass("Store Locator test Passed");
+                }
+                catch (AssertionException ex)
+                {
+                    TakeScreenShot();
+                    LogTestResult("Store Locator Test", "Store Locator Test failed", ex.Message);
+                    test.Fail("Store Locator test failed");
+                }
+            }
+        }
     }
 }
