0b077dac32f51b2da565d439d5860b8a2b9d6623
indcyr <148056333+indcyr@users.noreply.github.com>
2023-12-02 21:48:01 +0530

    Update FilterTest.cs

--- Lenskart/TestScripts/FilterTest.cs
+++ Lenskart/TestScripts/FilterTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using Lenskart.PageObjects;
+using Lenskart.Utilities;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace Lenskart.TestScripts
 {
-    internal class FilterTest
+    [TestFixture]
+    internal class FlterTest : CoreCodes
     {
+        [Test]
+        public void FilterSearchTest()
+        {
+            string? currDir = Directory.GetParent(@"../../../")?.FullName;
+            string filePath = currDir + "/Logs/log_" + DateTime.Now.ToString("yyyy-mm-dd_HH.mm.ss") + ".txt";
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .WriteTo.File(filePath, rollingInterval: RollingInterval.Day).CreateLogger();
+            LenskartHomePage lhp = new(driver);
+
+            try
+            {
+                var filterresult = lhp?.filterSearch();
+                var filteraddtocart = filterresult?.FilterGlasses();
+
+                List<string> str = driver.WindowHandles.ToList();
+                driver.SwitchTo().Window(str[1]);
+
+                filteraddtocart?.FilteredSearchAndaddToCart();
+                TakeScreenShot();
+                Assert.That(driver.Url, Does.Contain("cart"));
+                LogTestResult("Filter Test", "Filter Test Passed");
+                test = extent.CreateTest("Filter Test Passed");
+                test.Pass("Filter test Passed");
+            }
+            catch (AssertionException ex)
+            {
+                TakeScreenShot();
+                LogTestResult("Filter", "Filter Test failed", ex.Message);
+                test.Fail("Filter test failed");
+            }
+
+        }
     }
 }
