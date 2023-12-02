aacdbb189c7deb262078be9fcc632b0e29918ad3
indcyr <148056333+indcyr@users.noreply.github.com>
2023-12-02 21:45:13 +0530

    Update SearchResultsPage.cs

--- Lenskart/PageObjects/SearchResultsPage.cs
+++ Lenskart/PageObjects/SearchResultsPage.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Lenskart.PageObjects;
+using Lenskart.TestScripts;
+using MiniProj.PageObjects;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,28 @@ namespace Lenskart.PageObjects
 {
     internal class SearchResultsPage
     {
+        IWebDriver driver;
+        public SearchResultsPage(IWebDriver driver)
+        {
+            this.driver = driver ?? throw new ArgumentException(nameof(driver));
+            PageFactory.InitElements(driver, this);
+        }
+        [FindsBy(How = How.XPath, Using = "//div[@class='ProductContainer--1h5el3b bIsubZ'][1]")]
+        private IWebElement? SelectProduct { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "(//div[@class='ProductContainer--1h5el3b bIsubZ'])[position()=2]")]
+        private IWebElement? SelectSunglass { get; set; }
+
+        public SelectLensAndAddToCart ClickProduct()
+        {
+
+            SelectProduct?.Click();
+            return new SelectLensAndAddToCart(driver);
+        }
+        public SelectLensAndAddToCart SelectEyewear()
+        {
+            SelectSunglass?.Click();
+            return new SelectLensAndAddToCart(driver);
+        }
     }
 }
